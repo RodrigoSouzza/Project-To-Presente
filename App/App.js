cf67dddf17e6b5512d1328e066c792b066ec17e4
Rodrigo Souza <contato.rodrigoom@gmail.com>
2022-11-06 12:04:51 -0300

    instalando react-native elements e vector icons

--- App/App.js
+++ App/App.js
@@ -1,12 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import styles from './styles/mainStyle';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>tela inicial</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
