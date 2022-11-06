eef594511010d4d4f839e8768a3be0a5054234ce
Rodrigo Souza <contato.rodrigoom@gmail.com>
2022-11-06 11:46:20 -0300

    separando e generalizando styles

--- App/App.js
+++ App/App.js
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import styles from './styles/mainStyle';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>tela inicial</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#393B66',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
