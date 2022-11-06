67fb1b73ed72d2a4c86fff53ce38f64a6b2f0757
Rodrigo Souza <contato.rodrigoom@gmail.com>
2022-11-06 12:47:53 -0300

    criacao de tela de login exemplo

--- App/App.js
+++ App/App.js
@@ -1,10 +1,46 @@
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { Button, Input, Text} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import styles from './styles/mainStyle';
 
 export default function App() {
+
+  const [email, setEmail] = useState(null)
+  const[password, setPassword] = useState(null)
+
+  const entrar = () => {
+    console.log("entrou")
+    console.log(email)
+    console.log(password)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>tela inicial</Text>
+      <Text h3>Tô Presente</Text>
+      <Input
+      placeholder="E-mail"
+      leftIcon={{type: 'font-awesome', name: 'envelope'}}
+      onChangeText={value => setEmail(value)}
+      keyboardType="email-address"
+      />
+      <Input
+      placeholder="Sua senha"
+      leftIcon={{type: 'font-awesome', name: 'lock'}}
+      onChangeText={value => setPassword(value)}
+      secureTextEntry={true}
+      />
+      <Button 
+      icon={
+        <Icon
+        name="check"
+        size={15}
+        color="white"
+        />
+      }
+      title="Entrar"
+      onPress={() => entrar()}       
+      />      
     </View>
   );
 }
