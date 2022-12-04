cd1edf373fcd842fac6591c1912f4781241e7b5b
Rodrigo Souza <contato.rodrigoom@gmail.com>
2022-12-04 12:19:14 -0300

    implementacao de criptografia de senha

--- api/src/database/database.providers.ts
+++ api/src/database/database.providers.ts
@@ -9,7 +9,7 @@ export const databaseProviders = [
         host: 'localhost',
         port: 3306,
         username: 'root',
-        password: 'TwD7tF2017@',
+        password: 'TwD7tF2017',
         database: 'to_presente',
         entities: [
             __dirname + '/../**/*.entity{.ts,.js}',
--- api/src/user/user.service.ts
+++ api/src/user/user.service.ts
@@ -3,6 +3,7 @@ import { Injectable, Inject } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { UserRegisterDto } from './dto/user.register.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -21,7 +22,7 @@ export class UserService {
       user.name = data.name
       user.cpf = data.cpf
       user.email = data.email
-      user.password = data.password
+      user.password = bcrypt.hashSync(data.password,8)
 
       return this.userRepository.save(user)
       
